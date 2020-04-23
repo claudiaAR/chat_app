dbfbefba938da1c1c6d2f3dd9248d0e58a77c517
Claudia Aranda <claudia_15_@hotmail.com>
2020-04-23 15:31:06 +0200

    new update

---
+++ server/router.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('server is up and running')
+})
+
+module.export = router
+
+
+
+
+
+
+
+
+
+
+
