8f5beeeac0f061295fc3ac505e9dc4c1fb1d636c
Claudia Aranda <claudia_15_@hotmail.com>
2020-04-24 15:14:18 +0200

    its working 1:42

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 import './Chat.css'
+import InfoBar from '../InfoBar/InfoBar'
+import Input from '../Input/Input'
+import Messages from '../Messages/Messages'
 
 let socket;
 
@@ -52,20 +55,15 @@ const Chat = ({ location }) => {
     }
 
 
-    console.log( message, messages)
+    console.log(message, messages)
 
 
     return(
-        <div className="outerContanier">
-            <div className="contanier">
-            <InfoBar />
-
-
-                {/* <input 
-                    value={message} 
-                     onChange={(event) => setMessage(event.target.value)}
-                     onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}
-                 /> */}
+        <div className="outerContainer">
+            <div className="container">
+            <InfoBar room={room}/>
+            <Messages messages={messages} name={name}/>
+            <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
         </div>
         
--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,19 +1,24 @@
 import React from 'react'
+// import onlineIcon from '../../icons/onlineIcon.png'
+// import closedIcon from '../../icons/closedIcon.png' 
+
 import './InfoBar.css'
 
 
-const InfoBar = () => {
+const InfoBar = ({ room }) => (
     <div className="infoBar">
         <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="online image"  /> 
-            <h3>roomName</h3>
+            {/* <img className="onlineIcon" src={onlineIcon} alt="online image" /> */}
+            <p>online</p>
+            <h3>{room}</h3>
         </div>
         <div className="rightInnerContainer">
-           <a href="/"><img src={closedIcon} alt="close image" /></a> 
+           {/* <a href="/"><img src={closedIcon} alt="close image" /></a> */}
+           <a href="/"><p>closed</p></a>
         </div>
     </div>
 
 
-}
+)
 
 export default InfoBar
