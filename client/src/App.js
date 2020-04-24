94a7518424465c6f776badbeb85d44f39aae376a
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-04-24 09:06:51 +0200

    comments for client files: index.js+app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
+// This file is to set up the structure for our application
 import React from 'react'
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import Join from './components/Join'
 import Chat from './components/Chat'
 
-
-//the client will first se the Join-component
+//Router
+//URL: '/' displays the Join component (First thing the client will see on the website, the client will insert data)
+//URL: '/chat' displays the Chat component (When we have the data from the client, we will render the chat component)
 const App = () => (
     <Router>
         <Route path='/' exact component={Join} />
-        <Route path='/chat' exact component={Chat} />
+        <Route path='/chat' component={Chat} />
     </Router>
 )
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
+// This file initiate our application built with react. 
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//Imports the App component from App.js. 
 import App from './App'
 
-//passing the app-component to the root-element
+//Target our element in index.html with id: root and passing the App componenent to it.
 ReactDOM.render(<App />, document.querySelector('#root'))
 
 
