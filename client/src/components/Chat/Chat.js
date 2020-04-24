50d87b7ac2262630f587f80d4a87190bcec10c18
Claudia Aranda <claudia_15_@hotmail.com>
2020-04-24 12:03:57 +0200

    Co-authored-by: Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se> Co-authored-by: Pontus Tahir <pontus.tahir@gmail.com>

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,17 @@ const Chat = ({ location }) => {
 
         setName(name)
         setRoom(room)
-    })
+
+        socket.emit('join', {name, room}, () => {
+              
+        })
+        
+        return () => {
+            socket.emit('disconnect')
+
+            socket.off()
+        }
+    }, [ENDPOINT, location.search])
 
     return(
         <h1>Chat</h1>
---
+++ server/users.js
@@ -0,0 +1,41 @@
+const users = []
+
+const addUser = ({ id, name, room}) => {
+    // Javascript Mastery = javascriptmastery
+    //This is to know what string data we are using 
+    name = name.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+    
+    // Checking if the username is free.
+    const existingUser = user.find((user) => user.room === room && user.name === name )
+    //if the user try to log in with exicting name this will be trown out
+    if (existingUser) {
+     return { error: 'Username is taken' }
+    }
+    //if the user name dont have any conflicts the new user will be created in the user array
+    const user = { id, name, room }
+
+    users.push(user)
+
+    return { user }
+}
+
+
+const removeUser = (id) => {
+    // Finding the specific id of user.
+    const index = users.findIndex((user) => user.id === id)
+
+    //checking if user excist if so remove from user array
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+} 
+
+//if user excist it will be returned in the getUser variable
+const getUser = (id) => users.find((user) => user.id === id)
+
+
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room) 
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
